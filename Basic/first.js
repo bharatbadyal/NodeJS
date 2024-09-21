2e03c89492fa13c1104cc93306b70d5fe7b20ca4
Zhyan <myprof219@gmail.com>
2024-09-21 10:39:57 +0530

    First commit

---
+++ Basic/first.js
@@ -0,0 +1,5 @@
+const nameOfCourse = "Namaste NodeJs";
+// console.log(nameOfCourse);
+// console.log(global);
+console.log(this); // this give empty object here (in browser it gives global object  )
+console.log(globalThis);
